8d50c3680e25c2dfe648f2576c28195b537438e6
Daniel Hedlund <daniel@digitree.org>
2012-01-25 12:01:54 -0800

    Initial rails 3.1.3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vrmis::Application.config.secret_token = 'e3304555739b796ba91942d9d5df0c51a1044ed151c4f1191053e965bc4c4d2ddfedbed8ba6e534ad52ef905ef6981e9f0a83dff41a0324676f4554ec5d35a4e'
