c5fe5dfe1ed02c7bf53a521f1d97bca62fc3b1ce
Daniel Hedlund <daniel@digitree.org>
2012-01-25 12:08:32 -0800

    Support for overriding some settings locally in config/priv.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Vrmis::Application.config.secret_token = 'e3304555739b796ba91942d9d5df0c51a1044ed151c4f1191053e965bc4c4d2ddfedbed8ba6e534ad52ef905ef6981e9f0a83dff41a0324676f4554ec5d35a4e'
+token_file = Rails.root.join('config', 'priv', 'secret_token')
+unless token_file.exist?
+  token_file.open('w') {|f| f.write ActiveSupport::SecureRandom.hex(64) }
+end
+Vrmis::Application.config.secret_token = token_file.read
