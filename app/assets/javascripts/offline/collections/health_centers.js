977d82f6426c373f7d89588742b42e51c2e3108e
Daniel Hedlund <daniel@digitree.org>
2012-04-24 14:31:17 -0700

    Offline: remove get() override for health center collections

--- app/assets/javascripts/offline/collections/health_centers.js
+++ app/assets/javascripts/offline/collections/health_centers.js
@@ -7,11 +7,6 @@ Collections.HealthCenters = Backbone.Collection.extend({
     return hc.get('code');
   },
 
-  get: function (attr) {
-    if (typeof this[attr] == 'function') { return this[attr]() };
-    return Backbone.Model.prototype.get.call(this, attr);
-  },
-
   sum: function(attr) {
     return this.reduce(function(acc,hc) {
       var val = hc.get(attr);
